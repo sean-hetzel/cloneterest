6ab11de168aad2488bd010efc325621579c3db4c
Seanhetzel <seanhetzel1@gmail.com>
2019-08-19 22:13:19 -0700

    FIXED DYNAMIC RENDERING BITCHES!

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("%c DOM Content Loaded and Parsed!", "color: magenta");
-PORT = 3000 || process.env.PORT;
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("%c DOM Content Loaded and Parsed!", "color: magenta");
+PORT = 3000; // || process.env.PORT;
 
 // URL for json file of pins
 URL = `http://localhost:${PORT}/pins`;
 
+//////////////////////////////// FORM START ///////////////////////////////////
 // create form for posting a pin
 const add_pin_form = document.createElement("form");
+
+// create pin image url input
 const imageUrl = document.createElement("input");
-const imageName = document.createElement("input");
-const imageDesc = document.createElement("input");
-const submitButton = document.createElement("button");
-// adding attributes
 imageUrl.placeholder = "Image URL";
+
+// create pin name input
+const imageName = document.createElement("input");
 imageName.placeholder = "Name (optional)";
+
+// create pin description input
+const imageDesc = document.createElement("input");
 imageDesc.placeholder = "Description (optional)";
+
+// create submit button
+const submitButton = document.createElement("button");
 submitButton.textContent = "Share";
 submitButton.id = "submit_button";
+
 // appending form elements to add_pin_form
 add_pin_form.appendChild(imageUrl);
 add_pin_form.appendChild(imageName);
 add_pin_form.appendChild(imageDesc);
 add_pin_form.appendChild(submitButton);
+
 // add add_pin_form to new_pin_div
 document.getElementById("new_pin_div").appendChild(add_pin_form);
+
 // add event listener to submit button for add_pin_form and fetch post do add post function
 document.getElementById("new_pin_div").addEventListener("submit", event => {
     event.preventDefault();
@@ -35,6 +46,7 @@ document.getElementById("new_pin_div").addEventListener("submit", event => {
     addPost(postData);
     add_pin_form.reset(); // reset form
 });
+////////////////////////////////// FORM END ///////////////////////////////////
 
 // fetch post to add pin to pins.json database given to formData from add_pin_form
 function addPost(postData) {
@@ -49,7 +61,11 @@ function addPost(postData) {
             name: postData.name, // save name do database
             description: postData.description // save description do database
         })
-    });
+    })
+        .then(response => response.json())
+        .then(pinObj => {
+            renderPins(pinObj);
+        });
 }
 
 // fetch all image pins on page load
@@ -58,43 +74,50 @@ fetch(URL)
         return response.json();
     })
     .then(function(json) {
-        renderPins(json);
+        json.forEach(function(pin) {
+            renderPins(pin);
+        });
     });
 
 // render pins
-function renderPins(json) {
-    json.reverse().forEach(function(pin) {
-        // create pin card
-        const pinCard = document.createElement("div");
-        pinCard.classList.add("pin_card");
-        // create image with link to view larger
-        const pinImage = document.createElement("img");
-        pinImage.src = pin.url;
-        pinImage.alt = pin.name;
-        pinImage.onclick = function() {
-            window.location.href = pin.url;
-        };
-        // create pin name
-        const pinName = document.createElement("h2");
-        pinName.textContent = pin.name;
-        // create pin description
-        const pinDesc = document.createElement("p");
-        pinDesc.textContent = pin.description;
-        // create delete button with fetch to delete pin from pins.json database
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("delete_post");
-        deleteButton.textContent = "Delete";
-        deleteButton.addEventListener("click", () => {
-            fetch(`${URL}/${pin.id}`, { method: "DELETE" });
-        });
-        // append image, name, description & delete button to pin card
-        pinCard.appendChild(pinImage);
-        pinCard.appendChild(pinName);
-        pinCard.appendChild(pinDesc);
-        pinCard.appendChild(deleteButton);
-        // append pin to pins div
-        document.getElementById("pins_div").appendChild(pinCard);
+function renderPins(pin) {
+    // create pin card
+    const pinCard = document.createElement("div");
+    pinCard.classList.add("pin_card");
+
+    // create image with link to view larger
+    const pinImage = document.createElement("img");
+    pinImage.src = pin.url;
+    pinImage.alt = pin.name;
+    pinImage.onclick = function() {
+        window.location.href = pin.url;
+    };
+
+    // create pin name
+    const pinName = document.createElement("h2");
+    pinName.textContent = pin.name;
+
+    // create pin description
+    const pinDesc = document.createElement("p");
+    pinDesc.textContent = pin.description;
+
+    // create delete button with fetch to delete pin from pins.json database
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete_post");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+        fetch(`${URL}/${pin.id}`, { method: "DELETE" });
+        pinCard.remove();
     });
+
+    // append image, name, description & delete button to pin card
+    pinCard.appendChild(pinImage);
+    pinCard.appendChild(pinName);
+    pinCard.appendChild(pinDesc);
+    pinCard.appendChild(deleteButton);
+
+    // append pin to pins div
+    document.getElementById("pins_div").prepend(pinCard);
 }
 
-});
+// });
